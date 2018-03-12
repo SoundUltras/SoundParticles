688726203ffba870aaee9becc76a4386b9a0c7cd
mizun524 <mizun524@gmail.com>
2018-03-12 12:32:52 +0900

    add information

--- src/SubApp.cpp
+++ src/SubApp.cpp
@@ -13,9 +13,10 @@ void SubApp::update(){
 void SubApp::draw(){
     
     ofSetColor(225, 225, 225);
-    ofDrawBitmapString( displaymsg->currentModeStr, 0, 20);
-    ofDrawBitmapString( displaymsg->headpos, 0, 40);
-    ofDrawBitmapString( displaymsg->headrot, 0, 60);
-    ofDrawBitmapString( displaymsg->handpos, 0, 80);
+    ofDrawBitmapString( displaymsg->currentModeStr + "  1 or 2", 0, 20);
+    ofDrawBitmapString( displaymsg->cameraMode + "  k or h", 0, 40);
+    ofDrawBitmapString( displaymsg->headpos, 0, 60);
+    ofDrawBitmapString( displaymsg->headrot, 0, 80);
+    ofDrawBitmapString( displaymsg->handpos, 0, 100);
     
 }
